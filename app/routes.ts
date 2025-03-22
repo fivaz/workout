f13b1fe1134a0bce522d43c1d5126ae842140a3d
fivaz <s.a.fivaz@gmail.com>
2025-03-22 18:57:56 +0100

    add login component

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,5 @@ import { type RouteConfig, route, index, layout } from '@react-router/dev/routes
 
 export default [
 	route('login', 'routes/auth/login.tsx'),
-	route('register', 'routes/auth/register.tsx'),
-
 	layout('routes/dashboard/layout.tsx', [index('routes/dashboard/home.tsx')]),
 ] satisfies RouteConfig;
