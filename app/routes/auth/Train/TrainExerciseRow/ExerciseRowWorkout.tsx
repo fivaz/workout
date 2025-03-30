5090306d6c158357cf2b3b9fb9ea59f9f2e5257d
fivaz <s.a.fivaz@gmail.com>
2025-03-30 17:06:05 +0200

    refactor

--- app/routes/auth/Train/TrainExerciseRow/ExerciseRowWorkout.tsx
+++ app/routes/auth/Train/TrainExerciseRow/ExerciseRowWorkout.tsx
@@ -112,11 +112,11 @@ export function ExerciseRowWorkout({ exercise }: ExerciseRowWorkoutProps) {
 			<GButton color="none" className="w-full" onClick={handleAddNewSet}>
 				<div className="relative">
 					<div aria-hidden="true" className="absolute inset-0 flex items-center">
-						<div className="w-full border-t border-gray-300" />
+						<div className="w-full border-t border-gray-300 dark:border-gray-600" />
 					</div>
 					<div className="relative flex justify-center">
-						<span className="dark:bg-gray-900 bg-gray-50 px-2 text-gray-500">
-							<PlusIcon aria-hidden="true" className="size-4 text-gray-500" />
+						<span className="dark:bg-gray-900 bg-gray-50 px-2 text-gray-500 rounded-lg border border-gray-300 dark:border-gray-600">
+							<PlusIcon aria-hidden="true" className="size-4 text-gray-500 dark:text-gray-400" />
 						</span>
 					</div>
 				</div>
