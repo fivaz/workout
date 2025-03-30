c0233c77f26474f3b8020156ed5a3405efed94b0
fivaz <s.a.fivaz@gmail.com>
2025-03-30 15:50:54 +0200

    remain state of currentProject

--- app/routes/auth/AuthLayout.tsx
+++ app/routes/auth/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from 'react-router';
+import { NavLink, Outlet, useSearchParams } from 'react-router';
 import AuthProvider from '@/lib/auth/AuthProvider';
 import { ExerciseProvider } from '@/lib/exercise/ExerciseProvider';
 import { ToastContainer } from 'react-toastify';
@@ -8,6 +8,7 @@ import { ProgramProvider } from '@/lib/program/ProgramProvider';
 import { ROUTES } from '@/lib/consts';
 
 export default function AuthLayout() {
+	const [searchParams] = useSearchParams();
 	return (
 		<div className="flex flex-col min-h-screen">
 			<main className="flex-1">
@@ -31,15 +32,24 @@ export default function AuthLayout() {
 				</AuthProvider>
 			</main>
 			<footer className="flex items-center p-3 border-t  dark:text-gray-300 text-gray-800 dark:bg-gray-900 bg-gray-100 border-gray-300 dark:border-gray-800">
-				<NavLink to={ROUTES.PROGRAMS} className={clsx('text-sm flex flex-col items-center flex-1')}>
+				<NavLink
+					to={`${ROUTES.PROGRAMS}?${searchParams.toString()}`}
+					className={clsx('text-sm flex flex-col items-center flex-1')}
+				>
 					<ClipboardListIcon className="size-5" />
 					Programs
 				</NavLink>
-				<NavLink to={ROUTES.HOME} className={clsx('text-sm flex flex-col items-center flex-1')}>
+				<NavLink
+					to={`${ROUTES.HOME}?${searchParams.toString()}`}
+					className={clsx('text-sm flex flex-col items-center flex-1')}
+				>
 					<TimerIcon className="size-5" />
 					Train
 				</NavLink>
-				<NavLink to={ROUTES.STATS} className={clsx('text-sm flex flex-col items-center flex-1')}>
+				<NavLink
+					to={`${ROUTES.STATS}?${searchParams.toString()}`}
+					className={clsx('text-sm flex flex-col items-center flex-1')}
+				>
 					<TrophyIcon className="size-5" />
 					Stats
 				</NavLink>
--- app/routes/auth/Programs/ProgramRow/ProgramRow.tsx
+++ app/routes/auth/Programs/ProgramRow/ProgramRow.tsx
@@ -2,13 +2,17 @@ import GText from '@/components/GText';
 import { ProgramFormButton } from '@/routes/auth/Programs/ProgramFormButton';
 import { EllipsisVerticalIcon } from 'lucide-react';
 import type { Program } from '@/lib/program/program.model';
-import { NavLink } from 'react-router';
+import { NavLink, useSearchParams } from 'react-router';
 import { ROUTES } from '@/lib/consts';
 
 export function ProgramRow({ program }: { program: Program }) {
+	const [searchParams] = useSearchParams();
 	return (
 		<li className="flex gap-2 w-full hover-group hover:bg-gray-300 dark:hover:bg-gray-900 p-2 border border-gray-200 dark:border-gray-500 rounded-md bg-gray-100 dark:bg-gray-900">
-			<NavLink to={`${ROUTES.PROGRAMS}/${program.id}`} className="flex gap-2 flex-1">
+			<NavLink
+				to={`${ROUTES.PROGRAMS}/${program.id}?${searchParams.toString()}`}
+				className="flex gap-2 flex-1"
+			>
 				<div className="shrink-0 size-14 bg-green-500 rounded"></div>
 
 				<GText className="flex-1">{program.name}</GText>
