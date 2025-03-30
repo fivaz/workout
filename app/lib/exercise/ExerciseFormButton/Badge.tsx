5640d704d0a02eb7c165be30a6c134d1749bbc69
fivaz <s.a.fivaz@gmail.com>
2025-03-30 06:11:13 +0200

    refactor

---
+++ app/lib/exercise/ExerciseFormButton/Badge.tsx
@@ -0,0 +1,10 @@
+import type { Program } from '@/lib/program/program.model';
+import type { PropsWithChildren } from 'react';
+
+export default function Badge({ children }: PropsWithChildren) {
+	return (
+		<span className="inline-flex items-center gap-x-0.5 rounded-md dark:bg-blue-400/10 bg-blue-50 px-2 py-1 text-xs font-medium dark:text-blue-400 text-blue-700 ring-1 ring-blue-700/10 dark:ring-blue-400/30 ring-inset">
+			{children}
+		</span>
+	);
+}
