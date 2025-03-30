b83981651419d9c8183937bed7f6d2184b58a519
fivaz <s.a.fivaz@gmail.com>
2025-03-30 16:59:58 +0200

    make footer fixable

--- app/routes/auth/AuthLayout.tsx
+++ app/routes/auth/AuthLayout.tsx
@@ -1,17 +1,14 @@
-import { NavLink, Outlet, useSearchParams } from 'react-router';
+import { Outlet } from 'react-router';
 import AuthProvider from '@/lib/auth/AuthProvider';
 import { ExerciseProvider } from '@/lib/exercise/ExerciseProvider';
 import { ToastContainer } from 'react-toastify';
-import { ClipboardListIcon, TimerIcon, TrophyIcon } from 'lucide-react';
-import clsx from 'clsx';
 import { ProgramProvider } from '@/lib/program/ProgramProvider';
-import { ROUTES } from '@/lib/consts';
+import Footer from '@/routes/auth/Footer';
 
 export default function AuthLayout() {
-	const [searchParams] = useSearchParams();
 	return (
 		<div className="flex flex-col min-h-screen">
-			<main className="flex-1">
+			<main className="flex-1 overflow-y-auto pb-[65px]">
 				<ToastContainer
 					position="top-right"
 					autoClose={1000}
@@ -31,29 +28,7 @@ export default function AuthLayout() {
 					</ProgramProvider>
 				</AuthProvider>
 			</main>
-			<footer className="flex items-center p-3 border-t  dark:text-gray-300 text-gray-800 dark:bg-gray-900 bg-gray-100 border-gray-300 dark:border-gray-800">
-				<NavLink
-					to={`${ROUTES.PROGRAMS}?${searchParams.toString()}`}
-					className={clsx('text-sm flex flex-col items-center flex-1')}
-				>
-					<ClipboardListIcon className="size-5" />
-					Programs
-				</NavLink>
-				<NavLink
-					to={`${ROUTES.HOME}?${searchParams.toString()}`}
-					className={clsx('text-sm flex flex-col items-center flex-1')}
-				>
-					<TimerIcon className="size-5" />
-					Train
-				</NavLink>
-				<NavLink
-					to={`${ROUTES.STATS}?${searchParams.toString()}`}
-					className={clsx('text-sm flex flex-col items-center flex-1')}
-				>
-					<TrophyIcon className="size-5" />
-					Stats
-				</NavLink>
-			</footer>
+			<Footer />
 		</div>
 	);
 }
---
+++ app/routes/auth/Footer.tsx
@@ -0,0 +1,33 @@
+import { NavLink, useSearchParams } from 'react-router';
+import { ROUTES } from '@/lib/consts';
+import clsx from 'clsx';
+import { ClipboardListIcon, TimerIcon, TrophyIcon } from 'lucide-react';
+
+export default function Footer() {
+	const [searchParams] = useSearchParams();
+	return (
+		<footer className="fixed bottom-0 left-0 right-0 flex items-center p-3 border-t dark:text-gray-300 text-gray-800 dark:bg-gray-900 bg-gray-100 border-gray-300 dark:border-gray-800">
+			<NavLink
+				to={`${ROUTES.PROGRAMS}?${searchParams.toString()}`}
+				className={clsx('text-sm flex flex-col items-center flex-1')}
+			>
+				<ClipboardListIcon className="size-5" />
+				Programs
+			</NavLink>
+			<NavLink
+				to={`${ROUTES.HOME}?${searchParams.toString()}`}
+				className={clsx('text-sm flex flex-col items-center flex-1')}
+			>
+				<TimerIcon className="size-5" />
+				Train
+			</NavLink>
+			<NavLink
+				to={`${ROUTES.STATS}?${searchParams.toString()}`}
+				className={clsx('text-sm flex flex-col items-center flex-1')}
+			>
+				<TrophyIcon className="size-5" />
+				Stats
+			</NavLink>
+		</footer>
+	);
+}
