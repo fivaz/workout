a97b30854e8ae814b9e50b938b917f76f0075304
fivaz <s.a.fivaz@gmail.com>
2025-03-31 13:40:10 +0200

    use generate-pwa-assets manually

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+				<link rel="icon" href="/favicon.ico" sizes="48x48" />
+				<link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+				<link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
+
 				<Meta />
 				<Links />
 			</head>
--- vite.config.ts
+++ vite.config.ts
@@ -15,16 +15,34 @@ export default defineConfig({
 			registerType: undefined,
 			injectRegister: null,
 
-			pwaAssets: {
-				disabled: false,
-				config: true,
-			},
-
 			manifest: {
 				name: 'workout',
 				short_name: 'workout',
 				description: 'workout',
 				theme_color: '#2b7fff',
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+				],
 			},
 
 			devOptions: {
