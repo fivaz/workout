03bc963f2d4417314276e77519d9eeaf9eb6d24c
fivaz <s.a.fivaz@gmail.com>
2025-03-31 13:58:21 +0200

    remove vite pwa

--- app/root.tsx
+++ app/root.tsx
@@ -30,10 +30,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<title>Workout</title>
 
 				<link rel="icon" href="/favicon.ico" sizes="48x48" />
 				<link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
 				<link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
+				<link rel="manifest" href="/manifest.json" />
+				<meta name="theme-color" content="#2b7fff" />
 
 				<Meta />
 				<Links />
--- vite.config.ts
+++ vite.config.ts
@@ -2,55 +2,9 @@ import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { VitePWA } from 'vite-plugin-pwa';
 
 import Terminal from 'vite-plugin-terminal';
+
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		reactRouter(),
-		tsconfigPaths(),
-		Terminal(),
-		VitePWA({
-			registerType: undefined,
-			injectRegister: null,
-
-			manifest: {
-				name: 'workout',
-				short_name: 'workout',
-				description: 'workout',
-				theme_color: '#2b7fff',
-				icons: [
-					{
-						src: 'pwa-64x64.png',
-						sizes: '64x64',
-						type: 'image/png',
-					},
-					{
-						src: 'pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png',
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-					},
-					{
-						src: 'maskable-icon-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'maskable',
-					},
-				],
-			},
-
-			devOptions: {
-				enabled: false,
-				navigateFallback: 'index.html',
-				suppressWarnings: true,
-				type: 'module',
-			},
-		}),
-	],
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), Terminal()],
 });
